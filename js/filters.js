10c93dc68265eb2307360d27a9b1e56cf6db36e5
DESKTOP-OCNFR63\dkcf <danielcalatayudf@gmail.com>
2018-02-12 00:16:24 +0100

    filter.js added

---
+++ js/filters.js
@@ -0,0 +1,30 @@
+var $grid = $('.grid').isotope({
+  itemSelector: '.item',
+  layoutMode: 'fitRows',
+  getSortData: {
+    name: '.name',
+    symbol: '.symbol',
+    number: '.number parseInt',
+    category: '[data-category]',
+    weight: function( itemElem ) {
+      var weight = $( itemElem ).find('.weight').text();
+      return parseFloat( weight.replace( /[\(\)]/g, '') );
+    }
+  }
+});
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+  $(".grid").goTo('slow');
+});
+
+// change is-checked class on buttons
+// $('.button-group').each( function( i, buttonGroup ) {
+//   var $buttonGroup = $( buttonGroup );
+//   $buttonGroup.on( 'click', 'button', function() {
+//     $buttonGroup.find('.is-checked').removeClass('is-checked');
+//     $( this ).addClass('is-checked');
+//   });
+// });
