8496ee1aabf0e223e66b169c9adae85f8be1b325
David Goodridge <dcgs.dev@gmail.com>
2018-03-15 21:18:49 +0100

    Allow searching by coin name or symbol

--- js/marketcaps.js
+++ js/marketcaps.js
@@ -38,10 +38,7 @@ var table = $('#marketcaps-table').DataTable({
         {
             responsivePriority: 1,
             title: "Nombre",
-            render: function( data, type, row, meta) {
-                if ( type !== "display" ) { return data.symbol; }
-                return "<span class='marketcap-symbol'>" + data.symbol + "</span>"+"<br/><span class='marketcaps-coinname'>" + data.name + "</span>";
-            }
+            render: marketcapsRenderNameColumn
         },
         {
             responsivePriority: 6,
@@ -54,11 +51,7 @@ var table = $('#marketcaps-table').DataTable({
             className: "dt-right",
             render: function( data, type, row, meta) {
                 if ( type !== "display" ) { return data.price; }
-                if ( data.positiveChange) {
-                    return "<div class=\"marketcaps-pricechange-positive\">$" + data.price + "&nbsp;<span class=\"carot-icon\">▲</span></div>";
-                } else {
-                    return "<div class=\"marketcaps-pricechange-negative\">$" + data.price + "&nbsp;<span class=\"carot-icon\">▼</span></div>";
-                }
+                return marketcapsGeneratePriceHtml( data.price, data.positiveChange );
             }
         },
         {
@@ -72,13 +65,8 @@ var table = $('#marketcaps-table').DataTable({
             className: "dt-right",
             render: function ( data, type, row, meta ) {
                 if ( type !== "display" ) { return data; }
-                if ( data > 0) {
-                    return "<div class=\"marketcaps-pricechange-positive\">" + data + "%&nbsp;<span class=\"carot-icon\">▲</span></div>";
-                } else {
-                    return "<div class=\"marketcaps-pricechange-negative\">" + data + "%&nbsp;<span class=\"carot-icon\">▼</span></div>";
-                }
+                return marketcapsGeneratePriceChangeHtml( data );
             }
-            
         },
         {
             responsivePriority: 2,
@@ -86,11 +74,7 @@ var table = $('#marketcaps-table').DataTable({
             className: "dt-right",
             render: function ( data, type, row, meta ) {
                 if ( type !== "display" ) { return data; }
-                if ( data > 0) {
-                    return "<div class=\"marketcaps-pricechange-positive\">" + data + "%&nbsp;<span class=\"carot-icon\">▲</span></div>";
-                } else {
-                    return "<div class=\"marketcaps-pricechange-negative\">" + data + "%&nbsp;<span class=\"carot-icon\">▼</span></div>";
-                }
+                return marketcapsGeneratePriceChangeHtml( data );
             }
         },
         { //Columna espaciadora
@@ -136,3 +120,26 @@ $.get( "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=300", functio
         table.responsive.recalc();
     })
 });
+
+function marketcapsGeneratePriceChangeHtml( priceChange ) {
+    return marketcapsGeneratePriceHtml( priceChange, priceChange );
+}
+
+function marketcapsGeneratePriceHtml( price, priceChange ) {
+    if ( priceChange > 0) {
+        return "<div class=\"marketcaps-pricechange-positive\">" + price + "%&nbsp;<span class=\"carot-icon\">▲</span></div>";
+    } else {
+        return "<div class=\"marketcaps-pricechange-negative\">" + price + "%&nbsp;<span class=\"carot-icon\">▼</span></div>";
+    }
+}
+
+function marketcapsRenderNameColumn( data, type, row, meta ) {
+    if ( type === "filter" ) { return data.symbol+" "+data.name; }
+    else if ( type === "sort" ) { return data.symbol; }
+    else if ( type === "display" ) { return marketcapsGenerateNameColumnHtml( data.name, data.symbol ); }
+    else return data.symbol; 
+}
+
+function marketcapsGenerateNameColumnHtml( name, symbol ) {
+    return "<span class='marketcap-symbol'>" + symbol + "</span>"+"<br/><span class='marketcaps-coinname'>" + name + "</span>";
+}
