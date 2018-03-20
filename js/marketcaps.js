22f71eaa6e3f5c0c17964d07610cadfff61c3f06
David Goodridge <dcgs.dev@gmail.com>
2018-03-20 21:37:38 +0100

    Criptocurrencies 1h and 24h change % relative to BTC or ETH Criptocurrencies 1h and 24h change % relative to USD

--- js/marketcaps.js
+++ js/marketcaps.js
@@ -68,7 +68,7 @@ let marketcapDataArray = new Array(),
             className: "dt-right",
             render: function( data, type, row, meta) {
                 if ( type !== "display" ) { return data.price; }
-                if ( data.positiveChange > 0) {
+                if ( data.changeIsPositive ) {
                     return "<div class=\"marketcaps-pricechange-positive\">" + generateCurrencyValueHtml( data.price, marketcapCurrency ) + "&nbsp;<span class=\"carot-icon\">▲</span></div>";
                 } else {
                     return "<div class=\"marketcaps-pricechange-negative\">" + generateCurrencyValueHtml( data.price, marketcapCurrency ) + "&nbsp;<span class=\"carot-icon\">▼</span></div>";
@@ -88,7 +88,7 @@ let marketcapDataArray = new Array(),
             className: "dt-right",
             render: function ( data, type, row, meta ) {
                 if ( type !== "display" ) { return data; }
-                if ( data > 0) {
+                if ( data >= 0) {
                     return "<div class=\"marketcaps-pricechange-positive\">" + data + "%&nbsp;<span class=\"carot-icon\">▲</span></div>";
                 } else {
                     return "<div class=\"marketcaps-pricechange-negative\">" + data + "%&nbsp;<span class=\"carot-icon\">▼</span></div>";
@@ -102,7 +102,7 @@ let marketcapDataArray = new Array(),
             className: "dt-right",
             render: function ( data, type, row, meta ) {
                 if ( type !== "display" ) { return data; }
-                if ( data > 0) {
+                if ( data >= 0) {
                     return "<div class=\"marketcaps-pricechange-positive\">" + data + "%&nbsp;<span class=\"carot-icon\">▲</span></div>";
                 } else {
                     return "<div class=\"marketcaps-pricechange-negative\">" + data + "%&nbsp;<span class=\"carot-icon\">▼</span></div>";
@@ -120,46 +120,246 @@ let marketcapDataArray = new Array(),
         }
     ]
 });
+
+function marketcapTableLoad_eur() {
+    table.processing( true );
+    let currency = "EUR";
+    marketcapCurrency = currency;
+    let getUrl = "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=300"
+    marketcapDataArray = [];
+    $("#marketcaps-currency-select").val(currency);
+    $.get( getUrl, function( response ) {
+
+        $.each(response, function(index, coin) {
+            
+            let price_eur = parseFloat(coin["price_eur"]);
+            let percent_change_24h = parseFloat(coin["percent_change_24h"]);
+            let percent_change_1h = parseFloat(coin["percent_change_1h"]);
+            let market_cap_eur = parseFloat(coin["market_cap_eur"]);  
+            
+            let priceLength = 2;
+              
+            let colRank = coin.rank;
+            let colIcon = coin.symbol.toLowerCase();
+            let colName = {
+                    symbol: coin.symbol,
+                    name: coin.name
+                };  
+            let colMarketCap = Math.floor(market_cap_eur).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            let colPrice = {
+                price: price_eur.toFixed(priceLength).replace(/(\d)(?=(\d{3})+\.)/g, "$1,"),
+                changeIsPositive: ( percent_change_1h >= 0 )
+            };
+            let colTokens = Math.floor(coin.available_supply).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+            let colChange1h = percent_change_1h.toFixed(2);
+            let colChange24h = percent_change_24h.toFixed(2);
+            let colSpacer = null;
+            let marketcapDataRow = [colRank, colIcon, colName, colMarketCap, colPrice, colTokens, colChange1h, colChange24h, colSpacer];
+            marketcapDataArray.push(marketcapDataRow);
+        });
+        
+    })
+    .success(function(response) {
+        table.clear();
+        table.rows.add( marketcapDataArray );
+        table.draw();
+        table.columns.adjust();
+        table.responsive.recalc();
+    })
+    .always(function() {
+        table.processing( false );
+    });
+}
+
 function marketcapTableLoad( currency ) {
+    switch( currency ) {
+    case "EUR":
+        marketcapTableLoad_eur();
+        break;
+    case "USD":
+        marketcapTableLoad_usd();
+        break;
+    default:
+        marketcapTableLoad_criptos( currency );
+    }
+    //return symbol; 
+}
+
+function marketcapTableLoad_usd() {
+    table.processing( true );
+    let currency = "USD";
+    marketcapCurrency = currency;
+    let getUrl = "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=300"
+    marketcapDataArray = [];
+    $("#marketcaps-currency-select").val(currency);
+    $.get( getUrl, function( response ) {
+
+        $.each(response, function(index, coin) {
+            
+            let price_usd = parseFloat(coin["price_usd"]);
+            let percent_change_24h = parseFloat(coin["percent_change_24h"]);
+            let percent_change_1h = parseFloat(coin["percent_change_1h"]);
+            let market_cap_usd = parseFloat(coin["market_cap_usd"]);  
+            
+            let priceLength = 2;
+            //let priceCurrent = price_usd.toFixed(priceLength).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+              
+            let colRank = coin.rank;
+            let colIcon = coin.symbol.toLowerCase();
+            let colName = {
+                    symbol: coin.symbol,
+                    name: coin.name
+                };  
+            let colMarketCap = Math.floor(market_cap_usd).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            let colPrice = {
+                price: price_usd.toFixed(priceLength).replace(/(\d)(?=(\d{3})+\.)/g, "$1,"),
+                changeIsPositive: ( percent_change_1h >= 0 )
+            };
+            let colTokens = Math.floor(coin.available_supply).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+            let colChange1h = percent_change_1h.toFixed(2);
+            let colChange24h = percent_change_24h.toFixed(2);
+            let colSpacer = null;
+            let marketcapDataRow = [colRank, colIcon, colName, colMarketCap, colPrice, colTokens, colChange1h, colChange24h, colSpacer];
+            marketcapDataArray.push(marketcapDataRow);
+        });
+        
+    })
+    .success(function(response) {
+        table.clear();
+        table.rows.add( marketcapDataArray );
+        table.draw();
+        table.columns.adjust();
+        table.responsive.recalc();
+    })
+    .always(function() {
+        table.processing( false );
+    });
+}
+
+function marketcapTableLoad( currency ) {
+    switch( currency ) {
+    case "EUR":
+        marketcapTableLoad_eur();
+        break;
+    case "USD":
+        marketcapTableLoad_usd();
+        break;
+    default:
+        marketcapTableLoad_criptos( currency );
+    }
+    //return symbol; 
+}
+
+function marketcapTableLoad_criptos( currency ) {
     table.processing( true );
     marketcapCurrency = currency;
     let getUrl = "https://api.coinmarketcap.com/v1/ticker/?convert="+ currency +"&limit=300"
     marketcapDataArray = [];
-
     $("#marketcaps-currency-select").val(currency);
     $.get( getUrl, function( response ) {
-        if (window.location.pathname === '/cotizaciones/') {
-            $.each(response, function(index, coin) {
-                let colSpacer = null,
-                    priceLength = '',
-                    colRank = coin.rank,
-                    colIcon = coin.symbol.toLowerCase(),
-                    colName = {
-                        symbol: coin.symbol,
-                        name: coin.name
-                    },
-                    marketCapString = coin["market_cap_"+currency.toLowerCase()],
-                    colMarketCap = Math.floor(marketCapString).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
-                    colTokens = Math.floor(coin.available_supply).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
-                    priceString = coin["price_"+currency.toLowerCase()];
+        
+        let p0_24h_curr_usd = 1.0;
+        let p0_1h_curr_usd = 1.0;
+        $.each(response, function(index, coin) {
+            
+            let price_usd = parseFloat(coin["price_usd"]);
+            let price_coin = parseFloat(coin["price_"+currency.toLowerCase()]);
+            let percent_change_24h = parseFloat(coin["percent_change_24h"]);
+            let percent_change_1h = parseFloat(coin["percent_change_1h"]);
+            let currentCoinIsSelectedCurrency = ( coin["symbol"].toLowerCase()===currency.toLowerCase() );
+            
+            // 24h change
+            let pf_24h_coin_usd = price_usd;
+            let c_24h_coin_usd = percent_change_24h;
+            let f_24h_coin_usd = 1;
+            if (c_24h_coin_usd >= 0) {
+                f_24h_coin_usd = 1 + ( c_24h_coin_usd / 100 );
+            } else {
+                f_24h_coin_usd = 1 - ( c_24h_coin_usd / 100 );  
+            }
+            let p0_24h_coin_usd = pf_24h_coin_usd / f_24h_coin_usd;
+            if (currentCoinIsSelectedCurrency) {
+                p0_24h_curr_usd = p0_24h_coin_usd;
+                
+                console.log("symbol="+coin["symbol"].toLowerCase());
+                console.log("percent_change_24h="+percent_change_24h);
+                console.log("percent_change_1h="+percent_change_1h);
+                
+                
+            }
+            let pf_24h_coin_curr = price_coin;
+            let colChange24hTemp = pf_24h_coin_curr / p0_24h_coin_usd; 
+            
+            // 1h change
+            let pf_1h_coin_usd = price_usd;
+            let c_1h_coin_usd = percent_change_1h;
+            let f_1h_coin_usd = 1;
+            if (c_1h_coin_usd >= 0) {
+                f_1h_coin_usd = 1 + ( c_1h_coin_usd / 100 );
+            } else {
+                f_1h_coin_usd = 1 - ( c_1h_coin_usd / 100 );  
+            }
+            let p0_1h_coin_usd = pf_1h_coin_usd / f_1h_coin_usd;
+            if (currentCoinIsSelectedCurrency) {
+                p0_1h_curr_usd = p0_1h_coin_usd;
+            }
+            let pf_1h_coin_curr = price_coin;
+            let colChange1hTemp = pf_1h_coin_curr / p0_1h_coin_usd; 
+            
 
-                if ((currency !== 'USD') && (currency !== 'EUR')) {
-                    priceLength = 10;
-                } else {
-                    priceLength = 2;  
-                }
+            let priceLength = '';
+            if ((currency !== 'USD') && (currency !== 'EUR')) {
+                priceLength = 10;
+            } else {
+                priceLength = 2;  
+            }
+            let priceCurrent = price_usd.toFixed(priceLength).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+            let marketcapCoinString = coin["market_cap_"+currency.toLowerCase()];    
+            let colRank = coin.rank;
+            let colIcon = coin.symbol.toLowerCase();
+            let colName = {
+                    symbol: coin.symbol,
+                    name: coin.name
+                };  
+            let colMarketCap = Math.floor(marketcapCoinString).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            let colPrice = {
+                price: price_coin.toFixed(priceLength).replace(/(\d)(?=(\d{3})+\.)/g, "$1,"),
+                changeIsPositive: true
+            };
+            let colTokens = Math.floor(coin.available_supply).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+            let colChange1h = percent_change_1h.toFixed(1);
+            let colChange24h = percent_change_24h.toFixed(1);
+            let colSpacer = null;
+            let marketcapDataRow = [colRank, colIcon, colName, colMarketCap, colPrice, colTokens, colChange1hTemp, colChange24hTemp, colSpacer];
+            marketcapDataArray.push(marketcapDataRow);
+        });
+        for (i = 0; i < marketcapDataArray.length; i++) { 
+        
+            // 24h change
+            let factor_24h = marketcapDataArray[i][7] * p0_24h_curr_usd;
+            let change_24h = 0;
+            if ( factor_24h >= 1 ) {
+                change_24h = +( factor_24h - 1 ) * 100;
+            } else {
+                change_24h = -( 1 - factor_24h ) * 100;
+            }
+            marketcapDataArray[i][7] = change_24h.toFixed(2);
+            
+            
+            // 1h change
+            let factor_1h = marketcapDataArray[i][6] * p0_1h_curr_usd;
+            let change_1h = 0;
+            if ( factor_1h >= 1 ) {
+                change_1h = +( factor_1h - 1 ) * 100;
+            } else {
+                change_1h = -( 1 - factor_1h ) * 100;
+            }
+            marketcapDataArray[i][6] = change_1h.toFixed(2);
 
-                let colPrice = {
-                    price: parseFloat(priceString).toFixed(priceLength).replace(/(\d)(?=(\d{3})+\.)/g, "$1,"),
-                    positiveChange: (parseFloat(coin.percent_change_1h).toFixed(1) > 0)
-                },
-                colChange1h = parseFloat(coin.percent_change_1h).toFixed(1),
-                colChange24h = parseFloat(coin.percent_change_24h).toFixed(1),
-                marketcapDataRow = [colRank, colIcon, colName, colMarketCap, colPrice, colTokens, colChange1h, colChange24h, colSpacer];
-
-                marketcapDataArray.push(marketcapDataRow);
-            });
+            //changeIsPositive boolean
+            marketcapDataArray[i][4].changeIsPositive = ( change_1h >= 0 );
         }
+        
     })
     .success(function(response) {
         table.clear();
