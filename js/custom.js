e7eb16b3e0e4290afc2fd71dba4d957213d9bef6
SESA\d.calatayud <d.calatayud@partner.samsung.com>
2018-10-25 09:27:44 +0200

    scroll fix attempt

--- js/custom.js
+++ js/custom.js
@@ -123,7 +123,7 @@
     window.onscroll = function () {
       if (mobileAndTabletcheck()) {
           var currentScrollPos = window.pageYOffset;
-          if (prevScrollpos > currentScrollPos) {
+          if (prevScrollpos >= currentScrollPos) {
             document.getElementsByClassName('site-navigation')[0].style.top = '0';
             if (document.getElementsByTagName('progress')[0]) {
               document.getElementsByTagName('progress')[0].style.marginTop = '';  
