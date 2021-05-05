df679e04c8db663af2eba7e5a6800759e2e45da9
DESKTOP-OCNFR63\dkcf <danielcalatayudf@gmail.com>
2021-05-05 14:37:47 +0200

    calculator ad experiment, TODO: make sure it only works like this on mobile.

--- js/tp.js
+++ js/tp.js
@@ -1,20 +1,47 @@
-let src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-let id = "adsense";
-let callback = function() {
-	(adsbygoogle = window.adsbygoogle || []).push({
-	    google_ad_client: "ca-pub-1252171391624665",
-	    enable_page_level_ads: true
-	  });
-};
-function loadScript (src, id, callback) {
-    let script = document.createElement('script');
-    script.src = src; // URL for the third-party library being loaded.
-    //script.id = id; // e.g., googleMaps or stripe
-    script.defer = true; // make sure that browser will run script after page loaded
-    document.body.appendChild(script);
+var elements = document.getElementsByClassName('lazy-load-ad');
+if (!elements.length)
+    return;
 
-    script.onload = () => {
-      if (callback) callback(); // conditional callback
-    };
+var googleAdScriptAppended = false;
+
+window.addEventListener('scroll', function (e) {
+    var currentScroll = document.scrollingElement.scrollTop;
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        if ((currentScroll > element.getBoundingClientRect().top - 100)) {
+
+            if (!googleAdScriptAppended) {
+
+                googleAdScriptAppended = true;
+                var scriptElement = document.createElement("script");
+                scriptElement.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+                document.body.appendChild(scriptElement);
+            }
+
+            if (window.adsbygoogle) {
+                element.classList.remove('lazy-load-ad');
+                element.classList.add('lazy-loaded-ad');
+                element.innerHTML = getAdsenseCode();
+                loadAd();
+
+                elements = document.getElementsByClassName('lazy-load-ad');
+            }
+        }
+    }
+}, false);
+
+//TODO change this to your ad unit!
+var getAdsenseCode = function () {
+    return '<div class="addsense-add">' +
+              '<ins class="adsbygoogle white-ad"' +
+                    'data-ad-format="auto"' +
+                    'data-ad-client="ca-pub-1252171391624665"' +
+                    'data-ad-slot="1002456567">' + 
+              '</ins > ' +
+           '</div>';
 };
-loadScript(src, id, callback);
+
+var loadAd = function () {
+    (adsbygoogle = window.adsbygoogle || []).push({});
+};
