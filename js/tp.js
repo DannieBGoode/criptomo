f330644607a0d9415da9078dea641e68788d1a0e
DESKTOP-OCNFR63\dkcf <danielcalatayudf@gmail.com>
2021-05-05 17:43:45 +0200

    undid test

--- js/tp.js
+++ js/tp.js
@@ -66,7 +66,14 @@ else {
     scriptElement.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
     document.body.appendChild(scriptElement);
 
-
+    (adsbygoogle = window.adsbygoogle || []).onload = function () {
+        [].forEach.call(document.getElementsByClassName('adsbygoogle'), function () {
+            adsbygoogle.push({
+                google_ad_client: "ca-pub-1252171391624665",
+                enable_page_level_ads: true
+            });
+        });
+    }
 
     for (var i = 0; i <= elements.length; i++) {
         i = 0;
