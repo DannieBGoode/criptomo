63e1a200cd19a9c47d4a5b92e33c1fcd2f8f1794
DESKTOP-OCNFR63\dkcf <danielcalatayudf@gmail.com>
2021-09-01 15:22:40 +0200

    changed api provider

--- js/tp.js
+++ js/tp.js
@@ -87,21 +87,21 @@ else {
 }
 
 // Check if country is in Stormgain's allowed country list
-// function checkGEO() {
-//     fetch('http://ip-api.com/json')
-//         .then(response => response.json())
-//         .then(data => {
-//             if (countriesBlackList.includes(data.countryCode)) {
-//                 allowedCountry = false;
-//             } else {
-//                 console.log("true");
-//                 allowedCountry = true;    
-//             }
-//         })
-//         .catch(console.error);
+function checkGEO() {
+    fetch('https://extreme-ip-lookup.com/json/')
+        .then(response => response.json())
+        .then(data => {
+            if (countriesBlackList.includes(data.countryCode)) {
+                allowedCountry = false;
+            } else {
+                console.log("true");
+                allowedCountry = true;    
+            }
+        })
+        .catch(console.error);
 
-// }
+}
 
-let allowedCountry = true;
+let allowedCountry;
 const countriesBlackList = ["US", "CA", "JP", "AF", "BA", "GY", "IQ", "LA", "YE", "VU", "UG", "SY", "ET", "LK", "TT", "TN", "IR", "KP" ]
 checkGEO();
